6ed8b46fa0cc880c5f99a3cfdb4b119fac05115a
Ian Atha <thatha@thatha.org>
2009-12-10 00:35:34 -0800

    Adding GUI feedback via a ProcessMonitor.

--- src/main/java/com/episparq/svntrunks/SVNTrunks.java
+++ src/main/java/com/episparq/svntrunks/SVNTrunks.java
@@ -14,78 +14,115 @@ import org.tmatesoft.svn.core.internal.wc.DefaultSVNAuthenticationManager;
 import org.tmatesoft.svn.core.io.SVNRepository;
 import org.tmatesoft.svn.core.io.SVNRepositoryFactory;
 
+import javax.swing.*;
 import java.io.PrintStream;
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.concurrent.PriorityBlockingQueue;
 
 /**
  * @author Ian Atha <thatha@thatha.org>
  */
 public class SVNTrunks {
-    final String base;
-    final SVNRepository repository;
-    final DirectedGraph<String, DefaultEdge> graph = new DefaultDirectedGraph<String, DefaultEdge>(DefaultEdge.class);
+    private final ProgressMonitor monitor;
+    private final String base;
+    private final SVNRepository repository;
+    private final DirectedGraph<String, DefaultEdge> graph = new DefaultDirectedGraph<String, DefaultEdge>(DefaultEdge.class);
+    private final PriorityBlockingQueue<String> queue = new PriorityBlockingQueue<String>();
 
+    volatile int enqueued = 0;
+    volatile int solved = 0;
+    volatile boolean stopWorking = false;
 
-    private void findTrunks(String location) throws SVNException {
-        Collection<SVNDirEntry> entries = repository.getDir(location, -1, null, SVNDirEntry.DIRENT_KIND, (Collection) null);
+    private void enqueue(String s) {
+        enqueued++;
+        queue.add(s);
+    }
 
-        int structuralDirsFound = 0;
-        for (SVNDirEntry entry : entries) {
-            if (entry.getName().equals("trunk") || entry.getName().equals("tags") || entry.getName().equals("branches")) {
-                structuralDirsFound++;
-            }
-        }
+    private void findTrunks() throws SVNException {
+        String location;
+        while (((location = queue.poll()) != null) && !monitor.isCanceled()) {
+            solved++;
 
-        if (structuralDirsFound == 3) {
-            String previous = "/";
-            graph.addVertex(previous);
-            for (String segment : ((location + "/trunk").split("/"))) {
-                if (segment.length() > 0) {
-                    String current = previous + segment + "/";
-                    graph.addVertex(current);
-                    graph.addEdge(previous, current);
-                    previous = current;
-                }
-            }
-        } else {
-            int traversed = 0;
+            monitor.setMaximum(enqueued);
+            monitor.setProgress(solved);
+            monitor.setNote(String.format("(%2.0f%%) %s", (float) solved / enqueued * 100, location));
+
+            Collection<SVNDirEntry> entries = repository.getDir(location, -1, null, SVNDirEntry.DIRENT_KIND, (Collection) null);
+
+            int structuralDirsFound = 0;
             for (SVNDirEntry entry : entries) {
-                if (entry.getKind() == SVNNodeKind.DIR) {
-                    String nextLocation = location + "/" + entry.getRelativePath();
-                    traversed++;
-                    findTrunks(nextLocation);
+                if (entry.getName().equals("trunk") || entry.getName().equals("tags") || entry.getName().equals("branches")) {
+                    structuralDirsFound++;
                 }
             }
-            if (traversed == 0) {
-                // out.println("# " + location + " ends without a full [trunk, tags, branches]!");
+
+            if (structuralDirsFound == 3) {
+                String previous = "/";
+                graph.addVertex(previous);
+                for (String segment : ((location + "/trunk").split("/"))) {
+                    if (segment.length() > 0) {
+                        String current = previous + segment + "/";
+                        graph.addVertex(current);
+                        graph.addEdge(previous, current);
+                        previous = current;
+                    }
+                }
+            } else {
+                int traversed = 0;
+                for (SVNDirEntry entry : entries) {
+                    if (entry.getKind() == SVNNodeKind.DIR) {
+                        String nextLocation = location + "/" + entry.getRelativePath();
+                        traversed++;
+                        enqueue(nextLocation);
+                    }
+                }
+                if (traversed == 0) {
+                    // out.println("# " + location + " ends without a full [trunk, tags, branches]!");
+                }
             }
         }
     }
 
     public SVNTrunks(String repositoryURL, PrintStream out) throws SVNException {
-        SVNRepositoryFactoryImpl.setup();
+        monitor = new ProgressMonitor(null, String.format("%s", repositoryURL), "", 1, 1);
+        monitor.setMillisToDecideToPopup(0);
+        monitor.setMillisToPopup(0);
         base = repositoryURL;
-        repository = SVNRepositoryFactory.create(SVNURL.parseURIEncoded(base));
-        ISVNAuthenticationManager authManager = new DefaultSVNAuthenticationManager(
-                null, true, null, "", null, "");
-        repository.setAuthenticationManager(authManager);
-        findTrunks("");
 
-        out.println(String.format("svn co --depth=immediates %s && cd `basename !$`", base));
-        for (Iterator<String> iterator = new TopologicalOrderIterator<String, DefaultEdge>(graph); iterator.hasNext();) {
-            String entry = iterator.next();
-            if (!entry.equals("/")) {
-                String depth;
-                if (entry.endsWith("/trunk/")) {
-                    depth = "infinity";
-                } else {
-                    depth = "immediates";
+        try {
+            SVNRepositoryFactoryImpl.setup();
+            repository = SVNRepositoryFactory.create(SVNURL.parseURIEncoded(base));
+            ISVNAuthenticationManager authManager = new DefaultSVNAuthenticationManager(
+                    null, true, null, "", null, "");
+            repository.setAuthenticationManager(authManager);
+
+            enqueue("");
+            findTrunks();
+
+            if (!monitor.isCanceled()) {
+                out.println(String.format("svn co --depth=immediates %s && cd `basename !$`", base));
+                for (Iterator<String> iterator = new TopologicalOrderIterator<String, DefaultEdge>(graph); iterator.hasNext();) {
+                    String entry = iterator.next();
+                    if (!entry.equals("/")) {
+                        String depth;
+                        if (entry.endsWith("/trunk/")) {
+                            depth = "infinity";
+                        } else {
+                            depth = "immediates";
+                        }
+                        out.println(String.format("svn up --set-depth %s %s", depth, entry.replaceFirst(base, "")));
+                    }
                 }
-                out.println(String.format("svn up --set-depth %s %s", depth, entry.replaceFirst(base, "")));
+                out.println(String.format("svn up && svn st && cd .."));
             }
+            repository.closeSession();
+        } catch (SVNException e) {
+            JOptionPane.showMessageDialog(null, e.getLocalizedMessage());
+            throw e;
+        } finally {
+            monitor.close();
         }
-        out.println(String.format("svn up && svn st && cd .."));
     }
 
     public static void main(String[] args) throws Exception {
